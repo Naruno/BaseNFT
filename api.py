8588b3139349d739f6bc4b9bdb4475365f68673a
Onur Atakan ULUSOY <ataogan06@gmail.com>
2022-04-24 12:34:39 +0000

    Some improvements for api and system

--- api.py
+++ api.py
@@ -5,16 +5,20 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 import argparse
+from doctest import Example
 import sys
 import flask
 from flask import jsonify
 from flask import request
+from waitress import serve
+
+from hash_functions.text_to_hash import texttohash
 
 app = flask.Flask(__name__)
 
-@app.route("/nft/<sha256>", methods=["GET"])
-def nft(sha256):
-    return jsonify(sha256)
+@app.route("/nft/<text>", methods=["GET"])
+def nft_text(text):
+    return jsonify(texttohash(str(text)))
 
 def start():
     """
@@ -26,14 +30,13 @@ def start():
         "The NFT Creator"
     )
 
-    parser.add_argument("-p", "--port", type=int, help="Add new UNL node")
+    parser.add_argument("-p", "--port", default=8000, type=int, help="Add new UNL node")
 
     args = parser.parse_args()
 
-    if len(sys.argv) < 2:
-        serve(app, host="0.0.0.0", port=8000)
-    else:
-        serve(app, host="0.0.0.0", port=args.port)
+
+
+    serve(app, host="0.0.0.0", port=args.port)
 
 
 if __name__ == "__main__":
--- hash_functions/sha256.py
+++ hash_functions/sha256.py
@@ -1,3 +1,10 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 import hashlib
 ####################    CONVERT TO SHA348 ####################
 enterfilepath = "Screen Shot 2022-03-23 at 20.05.05.png";#for ex
